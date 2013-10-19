3743ea2b2fc1ace4115f4bbdd733c4348a6395ab
Aurel Canciu <aurelcanciu@gmail.com>
2013-10-19 15:57:57 +0300

    Preload app

--- config/puma.rb
+++ config/puma.rb
@@ -2,9 +2,11 @@
 
 require 'fileutils'
 
-directory '/app'
 environment 'production'
 bind 'unix:///tmp/puma.sock'
+threads 8,32
+workers 3
+preload_app!
 
 on_worker_boot do
   FileUtils.touch('/tmp/app-initialized')
